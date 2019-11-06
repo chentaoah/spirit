96b05d1e9451c8e596712c420092609e822b9325
chenTx <609580885@qq.com>
2019-11-06 15:49:55 +0800

    成功推导方法返回类型,但是不支持重载,也不支持泛型

--- shy-core/src/main/java/com/sum/shy/core/parser/FieldParser.java
+++ shy-core/src/main/java/com/sum/shy/core/parser/FieldParser.java
@@ -2,6 +2,7 @@ package com.sum.shy.core.parser;
 
 import java.util.List;
 
+import com.sum.shy.core.analyzer.InvokationResolver;
 import com.sum.shy.core.analyzer.TypeDerivator;
 import com.sum.shy.core.analyzer.VariableTracker;
 import com.sum.shy.core.api.Parser;
@@ -17,8 +18,10 @@ public class FieldParser implements Parser {
 	@Override
 	public int parse(Clazz clazz, String scope, List<Line> lines, int index, Line line, Stmt stmt) {
 
-		// 类型推导
+		// 变量追踪
 		VariableTracker.check(clazz, null, null, line, stmt);
+		// 方法返回值推算
+		InvokationResolver.check(clazz, stmt);
 		// 类型推导
 		Type type = TypeDerivator.getType(stmt);
 		// 变量名
--- shy-core/src/main/java/com/sum/shy/core/utils/ReflectUtils.java
+++ shy-core/src/main/java/com/sum/shy/core/utils/ReflectUtils.java
@@ -1,15 +1,34 @@
 package com.sum.shy.core.utils;
 
+import java.lang.reflect.Method;
+
 import com.sum.shy.core.entity.Type;
 
 public class ReflectUtils {
 
-	public static Type getReturnType(String clazzName, String methodName) {
-		// TODO Auto-generated method stub
+	/**
+	 * 不支持java重载 不支持java泛型
+	 * 
+	 * @param className
+	 * @param methodName
+	 * @return
+	 */
+	public static Type getReturnType(String className, String methodName) {
+		try {
+			Class<?> clazz = Class.forName(className);
+			for (Method method : clazz.getDeclaredMethods()) {
+				if (method.getName().equals(methodName)) {
+					String returnType = method.getReturnType().getSimpleName();
+					return new Type(returnType);
+				}
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
-	public static Type getReturnType(Type typeAtt, String clazzName, String methodName) {
+	public static Type getReturnType(Type type, String clazzName, String methodName) {
 		// TODO Auto-generated method stub
 		return null;
 	}
