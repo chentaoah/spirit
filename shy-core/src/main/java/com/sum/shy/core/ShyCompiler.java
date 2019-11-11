bd247c74730b6484de5273efbc60fc5afbb243bb
chenTx <609580885@qq.com>
2019-11-11 18:02:50 +0800

    提交

--- shy-core/src/main/java/com/sum/shy/core/ShyCompiler.java
+++ shy-core/src/main/java/com/sum/shy/core/ShyCompiler.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 import com.sum.shy.core.entity.Clazz;
 
-public class Compiler {
+public class ShyCompiler {
 
 	// 主方法
 	public static void main(String[] args) throws IOException {
