fc069f5dcba1d90d7efa47b8e3bf5c265d81d12c
chenTx <609580885@qq.com>
2019-11-15 17:03:40 +0800

    1

--- shy-core/src/main/java/com/sum/shy/core/ShyCompiler.java
+++ shy-core/src/main/java/com/sum/shy/core/ShyCompiler.java
@@ -84,22 +84,6 @@ public class ShyCompiler {
 
 	}
 
-	public static void recursiveFiles(Map<String, File> files, String packageStr, String path) {
-
-		File dir = new File(path);
-		if (!dir.isDirectory()) {
-			return;
-		}
-		// 包名
-		packageStr = packageStr + ("".equals(packageStr) ? "" : ".") + dir.getName();
-
-		for (File f : dir.listFiles()) {
-			if (f.isDirectory()) {// 递归
-				recursiveFiles(files, packageStr, f.getAbsolutePath());
-			} else if (f.isFile()) {// 如果是文件就添加
-				files.put(packageStr + "." + f.getName().replace(".shy", ""), f);
-			}
-		}
-	}
+	
 
 }
