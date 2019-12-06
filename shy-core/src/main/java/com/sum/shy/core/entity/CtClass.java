517765f9ce9c1c34bbe6c3fdc3f3c6c5b104d749
chenTx <609580885@qq.com>
2019-12-06 14:35:28 +0800

    支持java其他基本类型，包括byte等

--- shy-core/src/main/java/com/sum/shy/core/entity/CtClass.java
+++ shy-core/src/main/java/com/sum/shy/core/entity/CtClass.java
@@ -138,55 +138,96 @@ public class CtClass implements Annotated {
 	}
 
 	private String getBasicType(String typeName) {
+
 		switch (typeName) {
+		// 空类型
 		case "void":
 			return void.class.getName();
+		// 基本类型
 		case "boolean":
 			return boolean.class.getName();
-		case "boolean[]":
-			return boolean[].class.getName();
+		case "char":
+			return char.class.getName();
+		case "short":
+			return short.class.getName();
 		case "int":
 			return int.class.getName();
-		case "int[]":
-			return int[].class.getName();
 		case "long":
 			return long.class.getName();
-		case "long[]":
-			return long[].class.getName();
+		case "float":
+			return float.class.getName();
 		case "double":
 			return double.class.getName();
+		case "byte":
+			return byte.class.getName();
+		// 基本类型数组
+		case "boolean[]":
+			return boolean[].class.getName();
+		case "char[]":
+			return char[].class.getName();
+		case "short[]":
+			return short[].class.getName();
+		case "int[]":
+			return int[].class.getName();
+		case "long[]":
+			return long[].class.getName();
+		case "float[]":
+			return float[].class.getName();
 		case "double[]":
 			return double[].class.getName();
+		case "byte[]":
+			return byte[].class.getName();
+		// 包装类
 		case "Boolean":
 			return Boolean.class.getName();
-		case "Boolean[]":
-			return Boolean[].class.getName();
+		case "Character":
+			return Character.class.getName();
+		case "Short":
+			return Short.class.getName();
 		case "Integer":
 			return Integer.class.getName();
-		case "Integer[]":
-			return Integer[].class.getName();
 		case "Long":
 			return Long.class.getName();
-		case "Long[]":
-			return Long[].class.getName();
+		case "Float":
+			return Float.class.getName();
 		case "Double":
 			return Double.class.getName();
+		case "Byte":
+			return Byte.class.getName();
+		// 包装类数组
+		case "Boolean[]":
+			return Boolean[].class.getName();
+		case "Character[]":
+			return Character[].class.getName();
+		case "Short[]":
+			return Short[].class.getName();
+		case "Integer[]":
+			return Integer[].class.getName();
+		case "Long[]":
+			return Long[].class.getName();
+		case "Float[]":
+			return Float[].class.getName();
 		case "Double[]":
 			return Double[].class.getName();
+		case "Byte[]":
+			return Byte[].class.getName();
+		// 类
 		case "Object":
 			return Object.class.getName();
-		case "Object[]":
-			return Object[].class.getName();
 		case "String":
 			return String.class.getName();
-		case "String[]":
-			return String[].class.getName();
 		case "List":
 			return List.class.getName();
 		case "Map":
 			return Map.class.getName();
 		case "Exception":
 			return Exception.class.getName();
+		// 类数组
+		case "Object[]":
+			return Object[].class.getName();
+		case "String[]":
+			return String[].class.getName();
+
 		default:
 			return null;
 		}
