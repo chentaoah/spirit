b627071d3dca63e6a9b97e6db08699bd3a36b13c
chentao <chentao@localhost>
2019-12-06 23:03:08 +0800

    1

--- shy-core/src/test/java/com/sum/shy/test/Test.java
+++ shy-core/src/test/java/com/sum/shy/test/Test.java
@@ -1,32 +1,12 @@
 package com.sum.shy.test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Test {
 
 	public static void main(String[] args) {
-		int a = 100;
-		int b = 100;
-		Map<String, Boolean> map = new HashMap<>();
-		map.put("key", a == b);
-		map.put("key", 10 > 100);
-		synchronized (map) {
-
-		}
-		List<String> list = new ArrayList<String>();
-		list.add("word");
-		list.add("word");
-		list.add("word");
-		list.add("word");
-		list.add("word");
-		list.add("word");
-
-		if (list instanceof Object) {
-
-		}
+		String text = "(?<=\").*?(?=\")";
+		String test = "G_Father father = \"G_Father\" + G_Father()";
+		test = test.replaceAll("(?!((?<=\").*?(?=\")))\bG_Father\b", "Child");
+		System.out.println(test);
 
 	}
 
