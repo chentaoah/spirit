550f48ceeee5c9de1633a73bd1ce0374ce7b9966
chenTx <609580885@qq.com>
2019-12-18 18:06:59 +0800

    1

--- shy-core/src/main/java/com/sum/shy/core/clazz/impl/Variable.java
+++ shy-core/src/main/java/com/sum/shy/core/clazz/impl/Variable.java
@@ -2,18 +2,13 @@ package com.sum.shy.core.clazz.impl;
 
 import com.sum.shy.core.type.api.Type;
 
-public class Variable {
+public class Variable extends Param {
 
 	public String block;
 
-	public Type type;
-
-	public String name;
-
 	public Variable(String block, Type type, String name) {
+		super(type, name);
 		this.block = block;
-		this.type = type;
-		this.name = name;
 	}
 
 }
