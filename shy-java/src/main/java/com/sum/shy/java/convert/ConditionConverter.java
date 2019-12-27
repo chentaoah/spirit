9a423ad15155c769ad265e8aaef96165737f4662
chenTx <609580885@qq.com>
2019-12-27 13:45:47 +0800

    1

--- shy-java/src/main/java/com/sum/shy/java/convert/ConditionConverter.java
+++ shy-java/src/main/java/com/sum/shy/java/convert/ConditionConverter.java
@@ -6,64 +6,20 @@ import com.sum.shy.core.entity.Constants;
 import com.sum.shy.core.entity.Line;
 import com.sum.shy.core.entity.Stmt;
 import com.sum.shy.core.entity.Token;
-import com.sum.shy.lib.StringUtils;
+import com.sum.shy.java.JavaConverter;
 
 public class ConditionConverter extends DefaultConverter {
 
 	@Override
 	public Stmt convert(CtClass clazz, CtMethod method, String indent, String block, Line line, Stmt stmt) {
 
-		convertExpress(clazz, method, indent, block, line, stmt);
+		JavaConverter.convertCommon(clazz, stmt);
+		stmt = JavaConverter.convertEquals(clazz, stmt);// 这个比较特别，stmt的替换是通过处理Node实现的，其实是操作副本完成的
 
 		stmt.tokens.add(1, new Token(Constants.SEPARATOR_TOKEN, "(", null));
 		stmt.tokens.add(stmt.size() - 1, new Token(Constants.SEPARATOR_TOKEN, ")", null));
 
 		return stmt;
-	}
-
-	public static void convertExpress(CtClass clazz, CtMethod method, String indent, String block, Line line,
-			Stmt stmt) {
-		for (int i = 0; i < stmt.size(); i++) {
-			Token token = stmt.getToken(i);
-			// 如果是变量且是字符串类型
-			if (token.isVar() && token.getTypeAtt().isStr()) {
-				try {
-					Token nextToken = stmt.getToken(i + 1);
-					// 如果紧跟着==操作符
-					if (nextToken.isOperator() && "==".equals(nextToken.value)) {
-						Token paramToken = stmt.getToken(i + 2);
-						stmt.tokens.set(i, new Token(Constants.EXPRESS_TOKEN,
-								"StringUtils.equals(" + token.value + ", " + paramToken.value + ")", null));
-						// 移除原来的
-						stmt.tokens.remove(i + 1);
-						stmt.tokens.remove(i + 1);
-						// 添加依赖
-						clazz.addImport(StringUtils.class.getName());
-
-					} else if (nextToken.isOperator() && "!=".equals(nextToken.value)) {
-						// 如果紧跟着!=操作符
-						Token paramToken = stmt.getToken(i + 2);
-						stmt.tokens.set(i, new Token(Constants.EXPRESS_TOKEN,
-								"!StringUtils.equals(" + token.value + ", " + paramToken.value + ")", null));
-						// 移除原来的
-						stmt.tokens.remove(i + 1);
-						stmt.tokens.remove(i + 1);
-						// 添加依赖
-						clazz.addImport(StringUtils.class.getName());
-
-					} else {
-						// 只有一个孤零零的字符串,则说明是判空
-						stmt.tokens.set(i, new Token(Constants.EXPRESS_TOKEN,
-								"StringUtils.isNotEmpty(" + token.value + ")", null));
-						// 添加依赖
-						clazz.addImport(StringUtils.class.getName());
-					}
-
-				} catch (Exception e) {
-					// ignore
-				}
-			}
-		}
 	}
 
 }
