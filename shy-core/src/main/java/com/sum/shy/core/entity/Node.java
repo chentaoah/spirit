759913eec3dd60a33716b33956a472595954f23c
chenTx <609580885@qq.com>
2019-12-30 14:55:33 +0800

    1

--- shy-core/src/main/java/com/sum/shy/core/entity/Node.java
+++ shy-core/src/main/java/com/sum/shy/core/entity/Node.java
@@ -14,15 +14,7 @@ public class Node {
 
 	@Override
 	public String toString() {
-		String text = null;
-		if (token.isExpress()) {// 如果是手动添加的表达式,那么则直接用表达式来拼接字符串
-			text = token.value.toString();
-
-		} else {
-			Stmt stmt = token.getStmt();
-			text = stmt.format(token);
-		}
-		return "" + (left == null ? "" : left) + text + (right == null ? "" : right);
+		return "" + (left == null ? "" : left) + token + (right == null ? "" : right);
 	}
 
 }
