1205227018b6b5d3a0b9f27f6bc4e95f7bef3020
chenTx <609580885@qq.com>
2019-12-30 16:45:10 +0800

    1

--- shy-core/src/main/java/com/sum/shy/core/entity/Node.java
+++ shy-core/src/main/java/com/sum/shy/core/entity/Node.java
@@ -2,6 +2,9 @@ package com.sum.shy.core.entity;
 
 public class Node {
 
+	public static final String[] BINARY_OPERATOR = new String[] { "+", "-", "*", "/", "%", "==", "!=", "<", ">", "<=",
+			">=", "&&", "||" };
+
 	public Token token;
 
 	public Node left;
@@ -14,7 +17,38 @@ public class Node {
 
 	@Override
 	public String toString() {
-		return "" + (left == null ? "" : left) + token + (right == null ? "" : right);
+		return "" + (left == null ? "" : left) + format() + (right == null ? "" : right);
+	}
+
+	public String format() {
+		if (token.isOperator()) {
+			String value = token.toString();
+
+			if ("++".equals(value) || "--".equals(value)) {// 一元操作符
+				if (left != null)
+					return value + " ";
+				if (right != null)
+					return " " + value;
+
+			} else if ("!".equals(value)) {
+				return value;
+
+			}
+
+			for (String operator : BINARY_OPERATOR) {// 二元操作符
+				if (operator.equals(value))
+					return " " + value + " ";
+			}
+
+		} else if (token.isCast()) {// 类型转换
+			return token + " ";
+
+		} else if (token.isInstanceof()) {// 关键字
+			return " " + token + " ";
+		}
+
+		// 其他
+		return token.toString();
 	}
 
 }
