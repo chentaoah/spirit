0dbe125454f00669c45b6118428e104216b4a963
chenTx <609580885@qq.com>
2020-01-14 13:53:06 +0800

    1

--- shy-core/src/main/java/com/sum/shy/parser/DeclareParser.java
+++ shy-core/src/main/java/com/sum/shy/parser/DeclareParser.java
@@ -9,17 +9,22 @@ import com.sum.shy.core.entity.Line;
 import com.sum.shy.core.entity.Stmt;
 import com.sum.shy.parser.api.Parser;
 import com.sum.shy.type.CodeType;
+import com.sum.shy.type.api.Type;
 
 public class DeclareParser implements Parser {
 
 	@Override
 	public int parse(IClass clazz, String scope, List<Line> lines, int index, Line line, Stmt stmt) {
-
-		// 变量名
+		// 注解
+		List<String> annotations = Context.get().getAnnotations();
+		// 类型
+		Type type = new CodeType(clazz, stmt.getToken(0));
+		// 名称
 		String name = stmt.get(1);
-		// 这里不再直接推导类型
-		clazz.addField(new IField(Context.get().getAnnotations(), scope, new CodeType(clazz, stmt.getToken(0)), name,
-				new Stmt(name)));
+		// 字段
+		IField field = new IField(annotations, scope, type, name, null);
+		// 添加字段
+		clazz.addField(field);
 
 		return 0;
 	}
