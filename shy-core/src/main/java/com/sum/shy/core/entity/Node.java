4f99e962122b5d435242f4d0c85ed1f99de16c93
chenTx <609580885@qq.com>
2020-01-15 13:27:26 +0800

    1

--- shy-core/src/main/java/com/sum/shy/core/entity/Node.java
+++ shy-core/src/main/java/com/sum/shy/core/entity/Node.java
@@ -32,30 +32,24 @@ public class Node {
 	public String format() {
 		if (token.isOperator()) {
 			String value = token.toString();
-
 			if ("++".equals(value) || "--".equals(value)) {// 一元操作符
 				if (left != null)
 					return value + " ";
 				if (right != null)
 					return " " + value;
-
 			} else if ("!".equals(value)) {
 				return value;
-
 			}
-
 			for (String operator : BINARY_OPERATOR) {// 二元操作符
 				if (operator.equals(value))
 					return " " + value + " ";
 			}
-
 		} else if (token.isCast()) {// 类型转换
 			return token + " ";
 
 		} else if (token.isInstanceof()) {// 关键字
 			return " " + token + " ";
 		}
-
 		// 其他
 		return token.toString();
 	}
