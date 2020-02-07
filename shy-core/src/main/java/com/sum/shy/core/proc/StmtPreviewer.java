a96d28edd04078bdfd97a8aa5ecdcc285734e513
chenTx <609580885@qq.com>
2020-02-07 17:15:42 +0800

    1

--- shy-core/src/main/java/com/sum/shy/core/proc/StmtPreviewer.java
+++ shy-core/src/main/java/com/sum/shy/core/proc/StmtPreviewer.java
@@ -7,6 +7,20 @@ public class StmtPreviewer {
 
 	public static void preview(IClass clazz, Element element) {
 
+		if (element.isDeclare()) {// String text
+			
+		} else if (element.isDeclareAssign()) {// String text = "abc"
+
+		} else if (element.isAssign()) {// text = "abc"
+
+		} else if (element.isCatch()) {// }catch Exception e{
+
+		} else if (element.isForIn()) {// for item in list {
+
+		} else if (element.isFor()) {// for i=0; i<100; i++ {
+			
+		}
+
 	}
 
 }
