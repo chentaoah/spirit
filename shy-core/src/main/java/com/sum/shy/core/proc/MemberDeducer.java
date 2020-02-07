08b7f43988e3facbdab1aaafcd53890a8ac7d865
chenTx <609580885@qq.com>
2020-02-07 14:03:46 +0800

    1

--- shy-core/src/main/java/com/sum/shy/core/proc/MemberDeducer.java
+++ shy-core/src/main/java/com/sum/shy/core/proc/MemberDeducer.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import com.sum.shy.core.clazz.AbsMember;
 import com.sum.shy.core.clazz.IClass;
+import com.sum.shy.core.clazz.IField;
+import com.sum.shy.core.clazz.IMethod;
 import com.sum.shy.core.type.api.IType;
 
 public class MemberDeducer {
@@ -16,43 +18,16 @@ public class MemberDeducer {
 	}
 
 	public static IType visitMember(IClass clazz, AbsMember member) {
-		// 上锁
-//		member.lock();
-//		IType type = member.getType();
-//		if (type == null) {
-//			if (member instanceof IField) {
-//				Stmt stmt = ((IField) member).stmt;
-//				if (stmt != null && stmt.isAssign()) {
-//					VariableTracker.trackStmt(clazz, null, null, stmt.line, stmt.subStmt(2, stmt.size()));
-//					InvokeVisiter.visitStmt(clazz, stmt);
-//					type = FastDerivator.deriveStmt(clazz, stmt);
-//				}
-//			} else if (member instanceof IMethod) {// 如果是方法
-//
-//				Holder<IType> holder = new Holder<>(new CodeType(clazz, Constants.VOID_TYPE));
-//				MethodResolver.resolve(clazz, (IMethod) member, new Handler() {
-//					@Override
-//					public Object handle(IClass clazz, IMethod method, String indent, String block, Line line,
-//							Stmt stmt) {
-//						// 有效返回，才是返回
-//						if (stmt.isReturn()) {
-//							IType returnType = FastDerivator.deriveStmt(clazz, stmt.subStmt(1, stmt.size()));
-//							if (holder.obj.isVoid() || holder.obj.isObj()) {
-//								if (returnType != null)
-//									holder.obj = returnType;
-//							}
-//						}
-//						return null;
-//					}
-//				});
-//				type = holder.obj;
-//
-//			}
-//
-//		}
-		// 解锁
+		member.lock();
+		if (member.getType() == null) {
+			if (member instanceof IField) {
+
+			} else if (member instanceof IMethod) {
+
+			}
+		}
 		member.unLock();
-		return null;
+		return member.getType();
 	}
 
 }
