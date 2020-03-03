a5393c437d89e4a5a0ee734fb518a40459145f4b
chenTx <609580885@qq.com>
2020-03-03 13:50:12 +0800

    1

--- shy-core/src/main/java/com/sum/shy/core/MemberVisiter.java
+++ shy-core/src/main/java/com/sum/shy/core/MemberVisiter.java
@@ -1,22 +1,26 @@
 package com.sum.shy.core;
 
+import java.util.List;
 import java.util.Map;
 
 import com.sum.shy.core.clazz.AbsMember;
 import com.sum.shy.core.clazz.IClass;
 import com.sum.shy.core.clazz.IField;
 import com.sum.shy.core.clazz.IMethod;
-import com.sum.shy.core.clazz.IParameter;
 import com.sum.shy.core.clazz.Variable;
+import com.sum.shy.core.doc.Line;
 import com.sum.shy.core.type.api.IType;
 
 public class MemberVisiter {
 
 	public static void visit(Map<String, IClass> allClasses) {
-		for (IClass clazz : allClasses.values()) {
-			for (AbsMember member : clazz.getAllMembers())
-				visitMember(clazz, member);
-		}
+		for (IClass clazz : allClasses.values())
+			visit(clazz);
+	}
+
+	public static void visit(IClass clazz) {
+		for (AbsMember member : clazz.getAllMembers())
+			visitMember(clazz, member);
 	}
 
 	public static IType visitMember(IClass clazz, AbsMember member) {
@@ -52,8 +56,9 @@ public class MemberVisiter {
 
 	public class MethodContext {// 方法上下文
 		public IMethod method;
-		public Map<String, Variable> variables;
+		public List<Variable> variables;
 		public String blockId;// 块位置id
+		public Line line;
 	}
 
 }
--- shy-core/src/main/java/com/sum/shy/core/proc/VariableTracker.java
+++ shy-core/src/main/java/com/sum/shy/core/proc/VariableTracker.java
@@ -7,15 +7,12 @@ import com.sum.shy.core.clazz.IField;
 import com.sum.shy.core.clazz.IMethod;
 import com.sum.shy.core.clazz.IParameter;
 import com.sum.shy.core.clazz.Variable;
-import com.sum.shy.core.doc.Element;
-import com.sum.shy.core.doc.Line;
 import com.sum.shy.core.doc.Stmt;
 import com.sum.shy.core.doc.Token;
 import com.sum.shy.core.entity.Constants;
 import com.sum.shy.core.entity.Context;
 import com.sum.shy.core.type.CodeType;
 import com.sum.shy.core.type.api.IType;
-import com.sum.shy.core.visiter.api.Visiter;
 import com.sum.shy.lib.StringUtils;
 
 /**
@@ -59,19 +56,21 @@ public class VariableTracker {
 		if (Constants.THIS_KEYWORD.equals(name))
 			return new CodeType(clazz, clazz.getClassName(), clazz.getTypeName());// 这里可能是比较隐晦的逻辑，因为
 
-		// 先在最近的位置找变量
+		// 先在方法上下文中找
 		IMethod method = context.method;
 		if (method != null) {
 			// 如果成员变量和方法声明中都没有声明该变量,则从变量追踪器里查询
-			Variable variable = context.variables.get(context.blockId);
-			if (variable != null)
-				return variable.type;
+			for (Variable variable : context.variables) {
+				if (variable.blockId.equals(context.blockId) && variable.name.equals(name))
+					return variable.type;
+			}
 			// 如果在成员变量中没有声明,则查看方法内是否声明
 			for (IParameter parameter : method.getParameters()) {
 				if (parameter.name.equals(name))
 					return parameter.type;
 			}
 		}
+
 		// 成员变量
 		for (IField field : clazz.fields) {
 			if (field.getName().equals(name)) {
@@ -80,15 +79,16 @@ public class VariableTracker {
 				return field.type;
 			}
 		}
+
 		// 从继承里面去找
 		if (StringUtils.isNotEmpty(clazz.getSuperName())) {
-			Visiter visiter = Context.get().visiter;
-			return visiter.visitField(clazz, new CodeType(clazz, clazz.getSuperName()), name);
+			String className = clazz.findImport(clazz.getSuperName());
+			IClass father = Context.get().findClass(className);
+			return findType(father, null, name);
 		}
 
-		if (type == null)
-			throw new RuntimeException("Variable must be declared!number:[" + line.number + "], text:[ "
-					+ line.text.trim() + " ], var:[" + name + "]");
+		throw new RuntimeException("Variable must be declared!number:[" + context.line.number + "], text:[ "
+				+ context.line.text.trim() + " ], var:[" + name + "]");
 
 	}
 
