af47f0033e595c35399042a681e48bfe67a89a88
chenTx <609580885@qq.com>
2020-03-05 15:10:08 +0800

    1

--- shy-core/src/main/java/com/sum/shy/core/document/Node.java
+++ shy-core/src/main/java/com/sum/shy/core/document/Node.java
@@ -1,14 +1,6 @@
 package com.sum.shy.core.document;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.sum.shy.core.metadata.Symbol;
-import com.sum.shy.core.metadata.SymbolTable;
-
 public class Node {
-
-	public static final List<Symbol> BINARY_OPERATOR = SymbolTable.selectBinaryOperator();
 
 	public Token token;
 
@@ -18,56 +10,6 @@ public class Node {
 
 	public Node(Token token) {
 		this.token = token;
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		List<Node> nodes = getNodes();
-		for (int i = 0; i < nodes.size(); i++) {
-			Node node = nodes.get(i);
-			sb.append(node.format());
-		}
-		return sb.toString().trim();
-	}
-
-	public String format() {
-		if (token.isOperator()) {
-			String value = token.toString();
-			if ("++".equals(value) || "--".equals(value)) {// 一元操作符
-				if (left != null)
-					return value + " ";
-				if (right != null)
-					return " " + value;
-
-			} else if ("!".equals(value)) {
-				return value;
-			}
-
-			for (Symbol symbol : BINARY_OPERATOR) {// 二元操作符
-				if (symbol.value.equals(value))
-					return " " + value + " ";
-			}
-
-		} else if (token.isCast()) {// 类型转换
-			return token + " ";
-
-		} else if (token.isInstanceof()) {// 类型判断
-			return " " + token + " ";
-
-		}
-		// 其他
-		return token.toString();
-	}
-
-	public List<Node> getNodes() {
-		List<Node> nodes = new ArrayList<>();
-		nodes.add(this);
-		if (left != null)
-			nodes.addAll(0, left.getNodes());
-		if (right != null)
-			nodes.addAll(right.getNodes());
-		return nodes;
 	}
 
 }
