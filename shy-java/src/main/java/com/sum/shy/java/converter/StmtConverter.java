c96a8bfe004ec90b340610d4e06b82942bcae7ea
chentao <chentao@localhost>
2020-04-18 22:25:20 +0800

    1

--- shy-java/src/main/java/com/sum/shy/java/converter/StmtConverter.java
+++ shy-java/src/main/java/com/sum/shy/java/converter/StmtConverter.java
@@ -23,7 +23,7 @@ public class StmtConverter {
 
 		} else if (element.isFor()) {// for i=0; i<100; i++ {
 			Token token = element.getToken(1);
-			if (!token.isType() && token.isVar() && !token.isDerivedAtt())
+			if (!token.isType() && token.isVar() && token.isDerivedAtt())
 				element.addToken(1, new Token(Constants.TYPE_TOKEN, TypeBuilder.build(clazz, token.getTypeAtt())));
 
 		} else if (element.isForIn()) {// for item in list {
