794e954f4316053c3294460c080def8d31ecad59
chentao <609580885@qq.com>
2020-04-21 22:47:03 +0800

    添加空的map测试案例

--- shy-start/src/test/java/com/sum/test/type/Type.java
+++ shy-start/src/test/java/com/sum/test/type/Type.java
@@ -18,6 +18,7 @@ public class Type {
 	public double d = 100.0;
 	public String s = "string";
 	public List<Object> emptyList = Collection.newArrayList();
+	public Map<Object, Object> emptyMap = Collection.newHashMap();
 	public List<String> list = Collection.newArrayList("first", "second", "third");
 	public List<Object> list1 = Collection.newArrayList("string", 100);
 	public Map<String, Integer> map = Collection.newHashMap("key1", 123, "key2", 456);
