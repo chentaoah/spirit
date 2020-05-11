a63501551582d312ac7bb21f64f6464dec360fb1
chenTx <609580885@qq.com>
2020-05-11 17:15:15 +0800

    简化Token Stmt Element类型的代码

--- shy-core/src/main/java/com/sum/shy/core/document/Token.java
+++ shy-core/src/main/java/com/sum/shy/core/document/Token.java
@@ -4,11 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.sum.shy.core.clazz.IType;
+import com.sum.shy.core.document.api.Semantic;
 import com.sum.shy.core.entity.Constants;
-import com.sum.shy.core.metadata.SymbolTable;
 import com.sum.shy.lib.Assert;
 
-public class Token {
+public class Token extends Semantic {
 
 	public String type;
 
@@ -49,190 +49,22 @@ public class Token {
 	}
 
 	@Override
+	public String getType() {
+		return type;
+	}
+
+	@Override
+	public Object getValue() {
+		return value;
+	}
+
+	@Override
 	public String toString() {
 		return value.toString();
 	}
 
 	public String debug() {
 		return "<" + type + ", " + value + ">";
-	}
-
-	public boolean isAnnotation() {
-		return Constants.ANNOTATION_TOKEN.equals(type);
-	}
-
-	public boolean isKeyword() {
-		return Constants.KEYWORD_TOKEN.equals(type);
-	}
-
-	public boolean isOperator() {
-		return Constants.OPERATOR_TOKEN.equals(type);
-	}
-
-	public boolean isSeparator() {
-		return Constants.SEPARATOR_TOKEN.equals(type);
-	}
-
-	public boolean isType() {
-		return Constants.TYPE_TOKEN.equals(type);
-	}
-
-	public boolean isArrayInit() {
-		return Constants.ARRAY_INIT_TOKEN.equals(type);
-	}
-
-	public boolean isTypeInit() {
-		return Constants.TYPE_INIT_TOKEN.equals(type);
-	}
-
-	public boolean isNull() {
-		return Constants.NULL_TOKEN.equals(type);
-	}
-
-	public boolean isBool() {
-		return Constants.BOOL_TOKEN.equals(type);
-	}
-
-	public boolean isChar() {
-		return Constants.CHAR_TOKEN.equals(type);
-	}
-
-	public boolean isInt() {
-		return Constants.INT_TOKEN.equals(type);
-	}
-
-	public boolean isLong() {
-		return Constants.LONG_TOKEN.equals(type);
-	}
-
-	public boolean isDouble() {
-		return Constants.DOUBLE_TOKEN.equals(type);
-	}
-
-	public boolean isStr() {
-		return Constants.STR_TOKEN.equals(type);
-	}
-
-	public boolean isList() {
-		return Constants.LIST_TOKEN.equals(type);
-	}
-
-	public boolean isMap() {
-		return Constants.MAP_TOKEN.equals(type);
-	}
-
-	public boolean isSubexpress() {
-		return Constants.SUBEXPRESS_TOKEN.equals(type);
-	}
-
-	public boolean isCast() {
-		return Constants.CAST_TOKEN.equals(type);
-	}
-
-	public boolean isVar() {
-		return Constants.VAR_TOKEN.equals(type);
-	}
-
-	public boolean isLocalMethod() {
-		return Constants.LOCAL_METHOD_TOKEN.equals(type);
-	}
-
-	public boolean isVisitField() {
-		return Constants.VISIT_FIELD_TOKEN.equals(type);
-	}
-
-	public boolean isInvokeMethod() {
-		return Constants.INVOKE_METHOD_TOKEN.equals(type);
-	}
-
-	public boolean isVisitArrayIndex() {
-		return Constants.VISIT_ARRAY_INDEX_TOKEN.equals(type);
-	}
-
-	public boolean isArrayIndex() {
-		return Constants.ARRAY_INDEX_TOKEN.equals(type);
-	}
-
-	public boolean isPrefix() {
-		return Constants.PREFIX_TOKEN.equals(type);
-	}
-
-	public boolean isNode() {
-		return Constants.NODE_TOKEN.equals(type);
-	}
-
-	public boolean isCustomPrefix() {
-		return Constants.CUSTOM_PREFIX_TOKEN.equals(type);
-	}
-
-	public boolean isCustomSuffix() {
-		return Constants.CUSTOM_SUFFIX_TOKEN.equals(type);
-	}
-
-	public boolean isCustomExpress() {
-		return Constants.CUSTOM_EXPRESS_TOKEN.equals(type);
-	}
-
-	// =================== 复合判断 =====================
-
-	public boolean isInit() {
-		return isArrayInit() || isTypeInit();
-	}
-
-	public boolean isValue() {
-		return isNull() || isBool() || isChar() || isInt() || isLong() || isDouble() || isStr() || isList() || isMap();
-	}
-
-	public boolean isNumber() {
-		return isInt() || isLong() || isDouble();
-	}
-
-	public boolean isAccess() {
-		return isLocalMethod() || isVisitField() || isInvokeMethod() || isVisitArrayIndex() || isArrayIndex();
-	}
-
-	public boolean isInvoke() {
-		return isTypeInit() || isLocalMethod() || isInvokeMethod();
-	}
-
-	public boolean isFluent() {
-		return isVisitField() || isInvokeMethod() || isVisitArrayIndex();
-	}
-
-	public boolean canVisit() {
-		return isList() || isMap() || isSubexpress() || isInvoke();
-	}
-
-	public boolean isInstanceof() {
-		return isKeyword() && "instanceof".equals(value.toString());
-	}
-
-	public boolean isArithmetic() {
-		return isOperator() && SymbolTable.isArithmetic(value.toString());
-	}
-
-	public boolean isBitwise() {// 是否按位操作符
-		return isOperator() && SymbolTable.isBitwise(value.toString());
-	}
-
-	public boolean isRelation() {// 关系运算符
-		return isOperator() && SymbolTable.isRelation(value.toString());
-	}
-
-	public boolean isLogical() {// 是否判断的操作符,这些符号都会将value转化成boolean类型
-		return isOperator() && SymbolTable.isLogical(value.toString());
-	}
-
-	public boolean isAssign() {// 是否赋值操作符
-		return isOperator() && SymbolTable.isAssign(value.toString());
-	}
-
-	public boolean isEquals() {// 是否判断的操作符,这些符号都会将value转化成boolean类型
-		return isOperator() && ("==".equals(value) || "!=".equals(value));
-	}
-
-	public boolean isShift() {// 是否位移操作符
-		return isOperator() && ("<<".equals(value) || ">>".equals(value));
 	}
 
 	// =================== 类型 =====================
