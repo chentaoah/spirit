96355821d563f623be661f47d8bb945df69605d4
chenTx <609580885@qq.com>
2020-05-21 15:44:55 +0800

    注解的类型推导，还需要优化

--- shy-core/src/main/java/com/sum/shy/core/clazz/type/AdaptiveLinker.java
+++ shy-core/src/main/java/com/sum/shy/core/clazz/type/AdaptiveLinker.java
@@ -16,8 +16,11 @@ public class AdaptiveLinker {
 
 		Assert.notEmpty(fieldName, "Field name cannot be empty!");
 
-		if (Constants.CLASS_KEYWORD.equals(fieldName))
-			return StaticType.CLASS_TYPE;// xxx.class class是关键字
+		if (Constants.CLASS_KEYWORD.equals(fieldName)) {// xxx.class class是关键字
+			IType returnType = TypeFactory.create(Class.class);
+			returnType.getGenericTypes().add(type);
+			return returnType;
+		}
 
 		if (type.isArray() && Constants.ARRAY_LENGTH.equals(fieldName))
 			return StaticType.INT_TYPE;// 访问数组length直接返回int类型
@@ -35,8 +38,7 @@ public class AdaptiveLinker {
 		if (Constants.SUPER_KEYWORD.equals(methodName) || Constants.THIS_KEYWORD.equals(methodName))
 			return type;// super()和this()指代父类或者本身的构造函数，返回这个类本身
 
-		return !type.isNative() ? CodeLinker.visitMethod(type, methodName, parameterTypes)
-				: NativeLinker.visitMethod(type, methodName, parameterTypes);
+		return !type.isNative() ? CodeLinker.visitMethod(type, methodName, parameterTypes) : NativeLinker.visitMethod(type, methodName, parameterTypes);
 	}
 
 }
--- shy-start/src/main/java/com/sum/shy/test/ClassGenericTest.java
+++ shy-start/src/main/java/com/sum/shy/test/ClassGenericTest.java
@@ -1,9 +1,15 @@
 package com.sum.shy.test;
 
+@MyTest
 public class ClassGenericTest {
 
 	public Class<?> getClazz() {
 		return null;
+	}
+
+	public void getAnno() {
+		MyTest myTest = ClassGenericTest.class.getAnnotation(MyTest.class);
+		System.out.println(myTest);
 	}
 
 }
