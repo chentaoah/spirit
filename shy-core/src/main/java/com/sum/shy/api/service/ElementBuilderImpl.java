bb83ef8f50c4903f8e5a254622b1ffbdcf4264de
chenTx <609580885@qq.com>
2020-06-09 13:17:46 +0800

    1

--- shy-core/src/main/java/com/sum/shy/api/service/ElementBuilderImpl.java
+++ shy-core/src/main/java/com/sum/shy/api/service/ElementBuilderImpl.java
@@ -17,7 +17,7 @@ public class ElementBuilderImpl implements ElementBuilder {
 	@Override
 	public Element buildElement(Line line) {
 		try {
-			Element element = new Element(line);
+			Element element = new Element();
 			// 1.词法拆分
 			List<String> words = LexicalAnalyzer.getWords(line.text);
 			// 2.token流
