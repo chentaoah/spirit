f6be8c9cb5e2c445f0b2d268e551acb2bde914ae
chenTx <609580885@qq.com>
2020-06-23 13:36:12 +0800

    添加启动脚本

--- deploy/bin/startup.bat
+++ deploy/bin/startup.bat
@@ -1,4 +1,5 @@
 @echo off
 title Shy编译器 - JAVA
-java -Xms128m -Xmx128m -cp ".;../lib/*;../lib_dep/*" com.sum.shy.start.JavaStarter ../src ../target
+echo 当前路径为：%~dp0
+java -Xms128m -Xmx128m -cp ".;../lib/*;../lib_dep/*" com.sum.shy.start.JavaStarter %~dp0\..\src %~dp0\..\target
 @pause
