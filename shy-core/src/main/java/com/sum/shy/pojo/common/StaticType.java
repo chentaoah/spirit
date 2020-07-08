0a81e9a78413d13728a3b6f32e007a31a19abeab
chenTx <609580885@qq.com>
2020-07-08 16:52:09 +0800

    c

--- shy-core/src/main/java/com/sum/shy/pojo/common/StaticType.java
+++ shy-core/src/main/java/com/sum/shy/pojo/common/StaticType.java
@@ -19,29 +19,28 @@ public class StaticType {
 	public static final IType STRING_TYPE;
 
 	static {
-		VOID_TYPE = build("void", "void", "void", null, true, false, false, false, false);
-		BOOLEAN_TYPE = build("boolean", "boolean", "boolean", null, true, false, false, false, false);
-		CHAR_TYPE = build("char", "char", "char", null, true, false, false, false, false);
-		BYTE_TYPE = build("byte", "byte", "byte", null, true, false, false, false, false);
-		SHORT_TYPE = build("short", "short", "short", null, true, false, false, false, false);
-		INT_TYPE = build("int", "int", "int", null, true, false, false, false, false);
-		LONG_TYPE = build("long", "long", "long", null, true, false, false, false, false);
-		FLOAT_TYPE = build("float", "float", "float", null, true, false, false, false, false);
-		DOUBLE_TYPE = build("double", "double", "double", null, true, false, false, false, false);
-
-		OBJECT_TYPE = build("java.lang.Object", "Object", "java.lang.Object", null, false, false, false, false, false);
-		NULL_TYPE = build("java.lang.Object", "Object", "java.lang.Object", null, false, false, true/* null */, false, false);
-		WILDCARD_TYPE = build("java.lang.Object", "Object", "java.lang.Object", null, false, false, false, true/* wildcard */, false);
-		STRING_TYPE = build("java.lang.String", "String", "java.lang.String", null, false, false, false, false, true/* native */);
+		VOID_TYPE = build("void", "void", "void", true, false, false, false, false);
+		BOOLEAN_TYPE = build("boolean", "boolean", "boolean", true, false, false, false, false);
+		CHAR_TYPE = build("char", "char", "char", true, false, false, false, false);
+		BYTE_TYPE = build("byte", "byte", "byte", true, false, false, false, false);
+		SHORT_TYPE = build("short", "short", "short", true, false, false, false, false);
+		INT_TYPE = build("int", "int", "int", true, false, false, false, false);
+		LONG_TYPE = build("long", "long", "long", true, false, false, false, false);
+		FLOAT_TYPE = build("float", "float", "float", true, false, false, false, false);
+		DOUBLE_TYPE = build("double", "double", "double", true, false, false, false, false);
+		OBJECT_TYPE = build("java.lang.Object", "Object", "java.lang.Object", false, false, false, false, false);
+		NULL_TYPE = build("java.lang.Object", "Object", "java.lang.Object", false, false, true/* null */, false, false);
+		WILDCARD_TYPE = build("java.lang.Object", "Object", "java.lang.Object", false, false, false, true/* wildcard */, false);
+		STRING_TYPE = build("java.lang.String", "String", "java.lang.String", false, false, false, false, true/* native */);
 	}
 
-	public static IType build(String className, String simpleName, String typeName, String genericName, boolean isPrimitive, boolean isArray, boolean isNull,
-			boolean isWildcard, boolean isNative) {
+	public static IType build(String className, String simpleName, String typeName, boolean isPrimitive, boolean isArray, boolean isNull, boolean isWildcard,
+			boolean isNative) {
 		IType type = new IType();
 		type.setClassName(className);
 		type.setSimpleName(simpleName);
 		type.setTypeName(typeName);
-		type.setGenericName(genericName);
+		type.setGenericName(null);
 		type.setPrimitive(isPrimitive);
 		type.setArray(isArray);
 		type.setNull(isNull);
