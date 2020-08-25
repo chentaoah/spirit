a1a546107b154ed6560f01b018a66f75dc937a55
chenTx <609580885@qq.com>
2020-08-25 13:52:25 +0800

    c

--- spirit-core/src/main/java/com/sum/spirit/pojo/clazz/IClass.java
+++ spirit-core/src/main/java/com/sum/spirit/pojo/clazz/IClass.java
@@ -7,16 +7,19 @@ import com.sum.pisces.core.ProxyFactory;
 import com.sum.spirit.api.link.TypeFactory;
 import com.sum.spirit.lib.Assert;
 import com.sum.spirit.pojo.common.Constants;
+import com.sum.spirit.pojo.common.Context;
 import com.sum.spirit.pojo.common.TypeTable;
 import com.sum.spirit.pojo.element.Element;
 import com.sum.spirit.pojo.element.Token;
 import com.sum.spirit.utils.TypeUtils;
 
-public class IClass extends Importable {
+public class IClass {
 
 	public static TypeFactory factory = ProxyFactory.get(TypeFactory.class);
 
 	public String packageStr;
+
+	public List<Import> imports = new ArrayList<>();
 
 	public List<IAnnotation> annotations = new ArrayList<>();
 
@@ -25,6 +28,68 @@ public class IClass extends Importable {
 	public List<IField> fields = new ArrayList<>();
 
 	public List<IMethod> methods = new ArrayList<>();
+
+	public String findImport(String simpleName) {
+		// 如果是className，则直接返回
+		if (simpleName.contains("."))
+			return simpleName;
+
+		// 如果传进来是个数组，那么处理一下
+		String targetName = TypeUtils.getTargetName(simpleName);
+		boolean isArray = TypeUtils.isArray(simpleName);
+
+		// 1.首先先去引入里面找
+		for (Import imp : imports) {
+			if (imp.isMatch(targetName))
+				return !isArray ? imp.getClassName() : "[L" + imp.getClassName() + ";";
+		}
+
+		// 2.在所有类里面找，包括这个类本身也在其中
+		String className = Context.get().getClassName(targetName);
+		if (className != null)
+			return !isArray ? className : "[L" + className + ";";
+
+		// 3.如果是基本类型，基本类型数组，或者java.lang.下的类，则直接返回
+		className = TypeTable.getClassName(simpleName);
+		if (className != null)
+			return className;
+
+		// 如果一直没有找到就抛出异常
+		throw new RuntimeException("No import info found!simpleName:[" + simpleName + "]");
+	}
+
+	public boolean addImport(String className) {
+
+		// 如果是数组，则把修饰符号去掉
+		String targetName = TypeUtils.getTargetName(className);
+		String lastName = TypeUtils.getLastName(className);
+
+		// 1. 基本类型不添加和java.lang.包下不添加
+		if (TypeTable.isPrimitive(targetName) || targetName.equals("java.lang." + lastName))
+			return true;
+
+		// 2.如果是本身,不添加
+		if (getClassName().equals(targetName))
+			return true;
+
+		// 3.如果引入了，则不必再添加了
+		// 4.如果没有引入，但是typeName相同，则无法引入
+		for (Import imp : imports) {
+			if (!imp.hasAlias()) {
+				if (imp.getClassName().equals(targetName)) {
+					return true;
+
+				} else if (imp.getLastName().equals(lastName)) {
+					return false;
+				}
+			} else {// 如果是别名，则不用添加了
+				if (imp.getClassName().equals(className))
+					return false;
+			}
+		}
+		imports.add(new Import(targetName));
+		return true;
+	}
 
 	public boolean isInterface() {
 		return element.isInterface();
@@ -66,7 +131,6 @@ public class IClass extends Importable {
 		return TypeUtils.getTargetName(getTypeToken().toString());
 	}
 
-	@Override
 	public String getClassName() {
 		return packageStr + "." + getSimpleName();
 	}
