16edff7bd1ae19a59c5428b99c631b6766431c51
tao.chen1 <tao.chen1@yintech.cn>
2020-12-18 12:37:52 +0800

    完成接口定义

---
+++ spirit-starter-kit/src/main/java/com/sum/spirit/starter/kit/controller/KitController.java
@@ -0,0 +1,30 @@
+package com.sum.spirit.starter.kit.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.sum.spirit.starter.kit.common.Result;
+import com.sum.spirit.starter.kit.pojo.MethodInfo;
+
+@RequestMapping("/spirit/kit")
+public class KitController {
+
+	@PostMapping("/method")
+	@SuppressWarnings("unused")
+	public Result<List<MethodInfo>> method(@RequestParam Map<String, String> params) {
+		String fileName = params.get("fileName");
+		String methodName = params.get("methodName");
+		String variableName = params.get("variableName");
+		String incompleteName = params.get("incompleteName");
+		Integer lineNumber = Integer.valueOf(params.get("lineNumber"));
+		// 1.进行不完整编译
+		// 2.找到对应的类，并找到对应的方法，从指定行，反向查找变量
+		return Result.success(new ArrayList<MethodInfo>());
+	}
+
+}
---
+++ spirit-starter-kit/src/main/java/com/sum/spirit/starter/kit/pojo/MethodInfo.java
@@ -0,0 +1,5 @@
+package com.sum.spirit.starter.kit.pojo;
+
+public class MethodInfo {
+
+}
