52a84622dc514933f51a4e6cd016f9d0e85f5c5b
tao.chen1 <tao.chen1@yintech.cn>
2021-02-09 13:18:34 +0800

    去掉多余的注解

--- spirit-core-lexer/src/main/java/com/sum/spirit/lexer/entity/Region.java
+++ spirit-core-lexer/src/main/java/com/sum/spirit/lexer/entity/Region.java
@@ -1,12 +1,10 @@
 package com.sum.spirit.lexer.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Region {
