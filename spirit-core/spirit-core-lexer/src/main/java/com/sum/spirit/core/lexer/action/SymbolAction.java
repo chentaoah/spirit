a76c981cb5787a0b21c71a04c3bb7bd2b93ab014
tao.chen1 <tao.chen1@yintech.cn>
2021-02-18 16:34:34 +0800

    简化代码

--- spirit-core/spirit-core-lexer/src/main/java/com/sum/spirit/core/lexer/action/SymbolAction.java
+++ spirit-core/spirit-core-lexer/src/main/java/com/sum/spirit/core/lexer/action/SymbolAction.java
@@ -1,7 +1,5 @@
 package com.sum.spirit.core.lexer.action;
 
-import java.util.Map;
-
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -24,14 +22,13 @@ public class SymbolAction extends AbstractLexerAction {
 
 		LexerContext context = event.context;
 		StringBuilder builder = context.builder;
-		Map<String, String> replacedStrs = context.replacedStrs;
 
 		// 尝试获取两个字符，判断是否双字符符号
 		if (context.index + 1 < builder.length()) {
 			String str = builder.substring(context.index, context.index + 2);
 			if (SymbolEnum.isDoubleSymbol(str)) {
 				Region region = new Region(context.index, context.index + 2);
-				replaceStr(builder, region, "@symbol" + context.nameCount++, replacedStrs);
+				replaceStr(builder, region, "@symbol" + context.nameCount++, context.replacedStrs);
 				return;
 			}
 		}
@@ -40,7 +37,7 @@ public class SymbolAction extends AbstractLexerAction {
 		String str = builder.substring(context.index, context.index + 1);
 		if (SymbolEnum.isSingleSymbol(str)) {
 			Region region = new Region(context.index, context.index + 1);
-			replaceStr(builder, region, "@symbol" + context.nameCount++, replacedStrs);
+			replaceStr(builder, region, "@symbol" + context.nameCount++, context.replacedStrs);
 			return;
 		}
 
