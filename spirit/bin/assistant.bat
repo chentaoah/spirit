1579a90705fea7fa2eea18226218031196dadf8b
tao.chen1 <tao.chen1@yintech.cn>
2021-03-08 11:16:08 +0800

    重新添加lib_dep的加载

--- spirit/bin/assistant.bat
+++ spirit/bin/assistant.bat
@@ -1,5 +1,5 @@
 @echo off
 title SPIRIT-STARTER-KIT
 echo path:%~dp0
-java -Xms128m -Xmx128m -cp ".;../lib/*;../lib_web/*" com.sum.spirit.starter.kit.KitStarter --input=%~dp0..\src
+java -Xms128m -Xmx128m -cp ".;../lib/*;../lib_web/*;../lib_dep/*" com.sum.spirit.starter.kit.KitStarter --input=%~dp0..\src
 @pause
