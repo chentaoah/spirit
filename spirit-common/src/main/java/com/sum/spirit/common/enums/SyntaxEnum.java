f280f0122e3ef2468c0ce4e1cf9ad47d7b0e0882
tao.chen1 <tao.chen1@yintech.cn>
2021-03-18 15:11:24 +0800

    添加词法和语法的说明案例

--- spirit-common/src/main/java/com/sum/spirit/common/enums/SyntaxEnum.java
+++ spirit-common/src/main/java/com/sum/spirit/common/enums/SyntaxEnum.java
@@ -1,36 +1,44 @@
 package com.sum.spirit.common.enums;
 
+//import com.sum.spirit.example.Animal
+//@Animal
+//class Horse {
+//	name = "Jessie"
+//	func call() {
+//		return "I am Jessie!"
+//	}
+//}
 public enum SyntaxEnum {
-	IMPORT, // import
-	ANNOTATION, // @Annotation
-	INTERFACE, // interface
-	ABSTRACT, // abstract
-	CLASS, // class
+	IMPORT, // import com.sum.spirit.example.Animal
+	ANNOTATION, // @Animal
+	INTERFACE, // interface Horse {
+	ABSTRACT, // abstract Horse {
+	CLASS, // class Horse {
 	SUPER, // super()
 	THIS, // this()
-	DECLARE, // String text
-	DECLARE_ASSIGN, // String text = "hello"
-	ASSIGN, // text= "hello"
-	DECLARE_FUNC, // String hello() {
-	FUNC, // func hello(){
-	FIELD_ASSIGN, // var.text = "abc"
-	INVOKE, // var1.invoke()
-	RETURN, // return var1
-	IF, // if i > 100 {
-	ELSE_IF, // else if i < 100 {
+	DECLARE, // String name
+	DECLARE_ASSIGN, // String name = "Jessie"
+	ASSIGN, // name = "Jessie"
+	DECLARE_FUNC, // String call() {
+	FUNC, // func call() {
+	FIELD_ASSIGN, // horse.name = "Jessie"
+	INVOKE, // horse.call()
+	RETURN, // return "I am Jessie!"
+	IF, // if i > 0 {
+	ELSE_IF, // else if i < 0 {
 	ELSE, // else {
 	END, // }
-	FOR, // for
+	FOR, // for (i=0; i<100; i++) {
 	FOR_IN, // for item in list {
-	WHILE, // while i > 100 {
+	WHILE, // while i > 0 {
 	CONTINUE, // continue
 	BREAK, // break
 	TRY, // try {
 	CATCH, // catch Exception e {
 	FINALLY, // finally {
 	THROW, // throw RuntimeException(e)
-	SYNC, // sync
-	PRINT, // print
-	DEBUG, // debug
-	ERROR // error
+	SYNC, // sync horse {
+	PRINT, // print "Jessie"
+	DEBUG, // debug "Jessie"
+	ERROR // error "Jessie"
 }
