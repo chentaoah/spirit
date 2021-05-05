961f3ba5d4a67a4dcd432c84d8f666320035d9f6
chenT <chenT@DESKTOP-FOHJ34E.lan>
2021-05-05 11:48:31 +0800

    简化代码

--- spirit-core/spirit-core-lexer/src/main/java/com/sum/spirit/core/lexer/action/SymbolAction.java
+++ spirit-core/spirit-core-lexer/src/main/java/com/sum/spirit/core/lexer/action/SymbolAction.java
@@ -34,8 +34,8 @@ public class SymbolAction implements LexerAction {
 			}
 		}
 
-		String str = builder.substring(context.index, context.index + 1);
-		if (SymbolEnum.isSingleSymbol(str)) {
+		String text = builder.substring(context.index, context.index + 1);
+		if (SymbolEnum.isSingleSymbol(text)) {
 			Region region = new Region(context.index, context.index + 1);
 			return new LexerResult(State.BREAK, region);
 		}
