b41dfcdcf5ee6044b68185a6ec1384ed40d0816f
chentaoah <609580885@qq.com>
2021-07-01 22:27:51 +0800

    去掉多余的依赖

--- spirit-tools/spirit-maven-plugin/src/main/java/com/gitee/spirit/maven/plugin/SpiritCompileMojo.java
+++ spirit-tools/spirit-maven-plugin/src/main/java/com/gitee/spirit/maven/plugin/SpiritCompileMojo.java
@@ -3,8 +3,6 @@ package com.gitee.spirit.maven.plugin;
 import java.util.List;
 
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
