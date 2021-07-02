c3e7405f5df6849f4ebebe32e669c10bd01091b7
chentaoah <609580885@qq.com>
2021-07-02 21:11:56 +0800

    简化代码

--- spirit-core/spirit-core-class/src/main/java/com/gitee/spirit/core/clazz/entity/IType.java
+++ spirit-core/spirit-core-class/src/main/java/com/gitee/spirit/core/clazz/entity/IType.java
@@ -25,94 +25,88 @@ import lombok.NonNull;
 @AllArgsConstructor
 public class IType {
 
-	private String className;
-	private String simpleName;
-	private String typeName;
-	private String genericName;// T K
-	private boolean isPrimitive;// 是否原始类型
-	private boolean isArray;// 是否数组
-	private boolean isNull;// 是否空值
-	private boolean isWildcard;// 是否“?”
-	private boolean isNative;// 是否本地类型
-	private int modifiers;// 进行位运算后得到的修饰符
+    private String className;
+    private String simpleName;
+    private String typeName;
+    private String genericName;// T K
+    private boolean isPrimitive;// 是否原始类型
+    private boolean isArray;// 是否数组
+    private boolean isNull;// 是否空值
+    private boolean isWildcard;// 是否“?”
+    private boolean isNative;// 是否本地类型
+    private int modifiers;// 进行位运算后得到的修饰符
 
-	@NonNull
-	@Builder.Default
-	private List<IType> genericTypes = new ArrayList<>();// 泛型参数
+    @NonNull
+    @Builder.Default
+    private List<IType> genericTypes = new ArrayList<>();// 泛型参数
 
-	public boolean isTypeVariable() {
-		return StringUtils.isNotEmpty(genericName);
-	}
+    public boolean isTypeVariable() {
+        return StringUtils.isNotEmpty(genericName);
+    }
 
-	public boolean isGenericType() {
-		return genericTypes != null && genericTypes.size() > 0;
-	}
+    public boolean isGenericType() {
+        return genericTypes != null && genericTypes.size() > 0;
+    }
 
-	public IType toBox() {
-		IType boxType = CommonTypes.getBoxType(getClassName());
-		return boxType != null ? boxType : this;
-	}
+    public IType toBox() {
+        IType boxType = CommonTypes.getBoxType(getClassName());
+        return boxType != null ? boxType : this;
+    }
 
-	public String getTargetName() {// 返回真正的className,包括数组中的
-		return TypeUtils.getTargetName(getClassName());
-	}
+    public String getTargetName() {// 返回真正的className,包括数组中的
+        return TypeUtils.getTargetName(getClassName());
+    }
 
-	public IType toTarget() {
-		TypeFactory factory = SpringUtils.getBean(TypeFactory.class);
-		return factory.create(getTargetName());
-	}
+    public IType toTarget() {
+        TypeFactory factory = SpringUtils.getBean(TypeFactory.class);
+        return factory.create(getTargetName());
+    }
 
-	public IType withProtected() {
-		this.setModifiers(AccessLevelEnum.PROTECTED.value);
-		return this;
-	}
+    public IType withProtected() {
+        this.setModifiers(AccessLevelEnum.PROTECTED.value);
+        return this;
+    }
 
-	public IType withPrivate() {
-		this.setModifiers(AccessLevelEnum.PRIVATE.value);
-		return this;
-	}
+    public IType withPrivate() {
+        this.setModifiers(AccessLevelEnum.PRIVATE.value);
+        return this;
+    }
 
-	public IType lowerAccessLevel() {
-		if (modifiers == AccessLevelEnum.PRIVATE.value) {
-			modifiers = AccessLevelEnum.PROTECTED.value;
+    public IType lowerAccessLevel() {
+        if (modifiers == AccessLevelEnum.PRIVATE.value) {
+            modifiers = AccessLevelEnum.PROTECTED.value;
+        }
+        return this;
+    }
 
-		} else if (modifiers == AccessLevelEnum.PROTECTED.value) {
-			modifiers = AccessLevelEnum.PROTECTED.value;
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof IType)) {
+            return false;
+        }
+        IType typeToMatch = (IType) obj;
+        boolean flag = getClassName().equals(typeToMatch.getClassName());
+        if (flag) {
+            int count = 0;
+            for (IType genericType : getGenericTypes()) {
+                if (!genericType.equals(typeToMatch.getGenericTypes().get(count++))) {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        return flag;
+    }
 
-		} else if (modifiers == AccessLevelEnum.PUBLIC.value) {
-			modifiers = AccessLevelEnum.PUBLIC.value;
-		}
-		return this;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (!(obj instanceof IType)) {
-			return false;
-		}
-		IType typeToMatch = (IType) obj;
-		boolean flag = getClassName().equals(typeToMatch.getClassName());
-		if (flag) {
-			int count = 0;
-			for (IType genericType : getGenericTypes()) {
-				if (!genericType.equals(typeToMatch.getGenericTypes().get(count++))) {
-					flag = false;
-					break;
-				}
-			}
-		}
-		return flag;
-	}
-
-	@Override
-	public String toString() {// 只打印当前类型的信息，不包括泛型
-		if (isWildcard()) {
-			return "?";
-		}
-		if (isTypeVariable()) {
-			return getGenericName();
-		}
-		return getSimpleName();
-	}
+    @Override
+    public String toString() {// 只打印当前类型的信息，不包括泛型
+        if (isWildcard()) {
+            return "?";
+        }
+        if (isTypeVariable()) {
+            return getGenericName();
+        }
+        return getSimpleName();
+    }
 
 }
